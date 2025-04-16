ccd1819eb713fc966b4b72c994a74a4646a060c5
Maxime HEIM <maxime.heim2023@campus-eni.fr>
2025-04-16 16:17:49 +0200

    h

--- src/main/java/fr/eni/cave/bo/client/Utilisateur.java
+++ src/main/java/fr/eni/cave/bo/client/Utilisateur.java
@@ -8,6 +8,10 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @Entity
 @NoArgsConstructor
@@ -18,7 +22,7 @@ import lombok.experimental.SuperBuilder;
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Table(name = "CAV_USER")
 @Inheritance(strategy = InheritanceType.JOINED)
-public class Utilisateur {
+public class Utilisateur implements UserDetails {
 
 	@Id
 	@Column(name = "LOGIN", nullable = false, length = 255)
@@ -34,5 +38,36 @@ public class Utilisateur {
 	
 	@Column(name = "FIRST_NAME", nullable = false, length = 150)
 	private String prenom;
+	@Column(name = "ROLES", nullable = false, length = 150)
+	private String roles;
 
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return null;
+	}
+
+	@Override
+	public String getUsername() {
+		return pseudo;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return UserDetails.super.isAccountNonExpired();
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return UserDetails.super.isAccountNonLocked();
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return UserDetails.super.isCredentialsNonExpired();
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return UserDetails.super.isEnabled();
+	}
 }
